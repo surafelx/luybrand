dcc947b989e683d65510bf4d0e80a65297231829
luybrand <surafelyimam.sy@gmail.com>
2024-01-24 18:29:10 +0300

    Fixed Video

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useMemo, } from "react";
+import React, { useMemo,} from "react";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
@@ -6,20 +6,20 @@ import YouTube from 'react-youtube';
 
 const VideoPlayer = () => {
   const opts = {
-    height: "390",
-    width: "100%",
+    height: '400',
+    width: '100%', 
     playerVars: {
-      autoplay: 0
-    }
+      autoplay: 0,
+    },
   };
+
   const videoId = '6jBb1RFBnBY';
 
   return (
-    <div className="video-container flex justify-center rounded-xl h-full w-1/2">
+    <div className="video-container rounded-xl max-w-2xl">
       <YouTube videoId={videoId} opts={opts} />
       <style jsx>{`
         .video-container {
-          width: 100%
           border-radius: 10px; // Adjust the value as needed
           overflow: hidden; // Ensure the rounded corners are visible
         }
@@ -36,7 +36,7 @@ const Hero = ({
 
   return (
     <div
-      className="max-w-screen-full px-8 xl:px-16 mx-auto"
+      className="max-w-screen-xl px-8 xl:px-16 mx-auto"
       id="about"
     >
       <ScrollAnimationWrapper>
@@ -47,74 +47,70 @@ const Hero = ({
             <img
               src="assets/Logo.svg"
               alt="Logo"
-              style={{ height: '50px', margin: 'auto' }}
+              style={{ height: '50px', margin: 'auto' }} 
             />
           </div>
         </motion.div>
       </ScrollAnimationWrapper>
-      <ScrollAnimationWrapper>
-        <div className="pt-[20px] px-4 sm:px-10 pb-[112px]">
-          <ScrollAnimationWrapper>
-            <motion.div
-              className="w-full justify-center"
-              variants={scrollAnimation}
-            >
-              <h1 style={{ fontFamily: 'jiret' }} className="text-xl md:text-3xl text-center">
-                የማህበራዊ ሚዲያ በመጠቀም እና ትርፋማ እድሎችን በመክፈት የእራሳችሁን ገቢ ለመፍጠር እድሉን ተጠቀሙ !
-              </h1>
-            </motion.div>
-          </ScrollAnimationWrapper>
-          <ScrollAnimationWrapper className="pt-12 px-2 sm:px-4">
-            <motion.div
-              className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-4 justify-center"
-              variants={scrollAnimation}
-            >
-              <div className="flex w-full">
-                <motion.div className="h-full w-full" variants={scrollAnimation}>
-                  <div className="relative">
-                    <VideoPlayer />
+      <div className="pt-[20px] px-10 pb-[112px]">
+        <ScrollAnimationWrapper>
+          <motion.div
+            className="w-full justify-center"
+            variants={scrollAnimation}>
+            <h1 style={{ fontFamily: 'jiret' }} className="text-xl md:text-3xl text-center">
+              የማህበራዊ ሚዲያ በመጠቀም እና ትርፋማ እድሎችን በመክፈት የእራሳችሁን ገቢ ለመፍጠር እድሉን ተጠቀሙ !
+            </h1>
+          </motion.div>
+        </ScrollAnimationWrapper>
+        <ScrollAnimationWrapper className="pt-12 px-4">
+          <motion.div
+            className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-4 justify-center"
+            variants={scrollAnimation}>
+            <div className="flex w-full">
+              <motion.div className="h-full w-full" variants={scrollAnimation}>
+                <div className="relative">
+                  <VideoPlayer />
+                </div>
+              </motion.div>
+            </div>
+            <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
+              <p style={{ fontFamily: 'Poppins', textTransform: 'uppercase', fontWeight: '900', color: 'white', fontSize: '18px' }} >
+                Mastering the Art of Digital Influence
+              </p>
+              <p style={{ fontFamily: 'jiret', color: 'white', fontSize: '18px' }}>
+                የዲጂታል ተፅእኖ ጥበብን እንዴት ወደ ትርፍ መቀየር ይቻላል?
+              </p>
+              <ul style={{ fontFamily: 'jiret' }} className=" mt-1 mb-6 flex flex-col gap-4">
+                <li>
+                  <div className="flex gap-3 mt-4 ">
+                    <div>-</div>
+                    <div>የሚማርኩ እና ተፅእኖ ያላቸው ውጤቶች የሚያደርሱ አሳቢ መልዕክቶችን ፣ ፁሁፍዋችን መስራት ።</div>
                   </div>
-                </motion.div>
-              </div>
-              <div className="flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-                <p style={{ fontFamily: 'Poppins', textTransform: 'uppercase', fontWeight: '900', color: 'white', fontSize: '18px' }} >
-                  Mastering the Art of Digital Influence
-                </p>
-                <p style={{ fontFamily: 'jiret', color: 'white', fontSize: '18px' }}>
-                  የዲጂታል ተፅእኖ ጥበብን እንዴት ወደ ትርፍ መቀየር ይቻላል?
-                </p>
-                <ul style={{ fontFamily: 'jiret' }} className="mt-1 mb-6 flex flex-col gap-4">
-                  <li>
-                    <div className="flex gap-3 mt-4 ">
-                      <div>-</div>
-                      <div>የሚማርኩ እና ተፅእኖ ያላቸው ውጤቶች የሚያደርሱ አሳቢ መልዕክቶችን ፣ ፁሁፍዋችን መስራት ።</div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="flex gap-3 ">
-                      <div>-</div>
-                      <div className="">በተሻሻለ ማህበራዊ ሚዲያ በመገኘት ።</div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="flex gap-3 ">
-                      <div>-</div>
-                      <div>እሴት በመፍጠር እና አቅርቦቶችን በብቃት ለገበያ በማቅረብ ።</div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="flex gap-3 ">
-                      <div>-</div>
-                      <div>በለውጥ ስትራቴጂ ሽያጮችን ከፍ በማድረግ ።</div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </motion.div>
-          </ScrollAnimationWrapper>
-        </div>
-      </ScrollAnimationWrapper>
+                </li>
+                <li>
+                  <div className="flex gap-3 ">
+                    <div>-</div>
+                    <div className="">በተሻሻለ ማህበራዊ ሚዲያ በመገኘት ።</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex gap-3 ">
+                    <div>-</div>
+                    <div>እሴት በመፍጠር እና አቅርቦቶችን በብቃት ለገበያ በማቅረብ ።</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex gap-3 ">
+                    <div>-</div>
+                    <div>በለውጥ ስትራቴጂ ሽያጮችን ከፍ በማድረግ ።</div>
+                  </div>
+                </li>
+              </ul>
+            </div>
 
+          </motion.div>
+        </ScrollAnimationWrapper>
+      </div> 
 
     </div >
   );
