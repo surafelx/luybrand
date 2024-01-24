ba2257383e04df6fab91b60e25dba6524cddc2dd
luybrand <surafelyimam.sy@gmail.com>
2024-01-24 18:54:19 +0300

    rounded xl

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useMemo,} from "react";
+import React, { useMemo, } from "react";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
@@ -7,7 +7,15 @@ import YouTube from 'react-youtube';
 const VideoPlayer = () => {
   const opts = {
     height: '400',
-    width: '100%', 
+    width: '600',
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const opts2 = {
+    height: '200',
+    width: '400',
     playerVars: {
       autoplay: 0,
     },
@@ -16,15 +24,29 @@ const VideoPlayer = () => {
   const videoId = '6jBb1RFBnBY';
 
   return (
-    <div className="video-container rounded-xl max-w-2xl">
-      <YouTube videoId={videoId} opts={opts} />
-      <style jsx>{`
-        .video-container {
-          border-radius: 10px; // Adjust the value as needed
-          overflow: hidden; // Ensure the rounded corners are visible
-        }
-      `}</style>
-    </div>
+    <>
+      <div>
+        <div className="video-container hidden lg:flex justify-center rounded-xl">
+          <YouTube className="rounded-xl" iframeClassName="rounded-xl" videoId={videoId} opts={opts} />
+          <style jsx>{`
+     .video-container {
+       border-radius: 10px; // Adjust the value as needed
+       overflow: hidden; // Ensure the rounded corners are visible
+     }
+   `}</style>
+        </div>
+      </div>
+      <div className="video-container lg:hidden flex justify-center rounded-xl">
+        <YouTube videoId={videoId} opts={opts2} />
+        <style jsx>{`
+     .video-container {
+       border-radius: 10px; // Adjust the value as needed
+       overflow: hidden; // Ensure the rounded corners are visible
+     }
+   `}</style>
+      </div>
+    </>
+
   );
 };
 
@@ -47,12 +69,12 @@ const Hero = ({
             <img
               src="assets/Logo.svg"
               alt="Logo"
-              style={{ height: '50px', margin: 'auto' }} 
+              style={{ height: '50px', margin: 'auto' }}
             />
           </div>
         </motion.div>
       </ScrollAnimationWrapper>
-      <div className="pt-[20px] px-10 pb-[112px]">
+      <div className="pt-[20px] px-10">
         <ScrollAnimationWrapper>
           <motion.div
             className="w-full justify-center"
@@ -64,7 +86,7 @@ const Hero = ({
         </ScrollAnimationWrapper>
         <ScrollAnimationWrapper className="pt-12 px-4">
           <motion.div
-            className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-4 justify-center"
+            className="flex justify-center h-full w-full flex-col xl:flex-row gap-8 xl:gap-0"
             variants={scrollAnimation}>
             <div className="flex w-full">
               <motion.div className="h-full w-full" variants={scrollAnimation}>
@@ -73,14 +95,14 @@ const Hero = ({
                 </div>
               </motion.div>
             </div>
-            <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-              <p style={{ fontFamily: 'Poppins', textTransform: 'uppercase', fontWeight: '900', color: 'white', fontSize: '18px' }} >
+            <div className=" flex flex-col justify-center">
+              <p style={{ fontFamily: 'Poppins', textTransform: 'uppercase', textAlign: 'center', fontWeight: '900', color: 'white', fontSize: '18px' }} >
                 Mastering the Art of Digital Influence
               </p>
-              <p style={{ fontFamily: 'jiret', color: 'white', fontSize: '18px' }}>
+              <p style={{ fontFamily: 'jiret', color: 'white', textAlign: 'center', fontSize: '18px' }}>
                 የዲጂታል ተፅእኖ ጥበብን እንዴት ወደ ትርፍ መቀየር ይቻላል?
               </p>
-              <ul style={{ fontFamily: 'jiret' }} className=" mt-1 mb-6 flex flex-col gap-4">
+              <ul style={{ fontFamily: 'jiret' }} className=" mt-1 mb-6 mx-auto flex flex-col gap-4">
                 <li>
                   <div className="flex gap-3 mt-4 ">
                     <div>-</div>
@@ -110,7 +132,7 @@ const Hero = ({
 
           </motion.div>
         </ScrollAnimationWrapper>
-      </div> 
+      </div>
 
     </div >
   );
